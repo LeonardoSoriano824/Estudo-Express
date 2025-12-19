2dec13905c048446272d0d104ec1718e5ef1d5fe
Leo <leonardo.sobrinho864@gmail.com>
2025-12-18 22:54:14 -0300

    Estudo 18/12/2025 Adicionando rotas DELETE

--- index.js
+++ index.js
@@ -29,6 +29,106 @@ app.get("/produtos", (req, res) => {
     res.json(produtos)
 })
 
+app.post("/produtos", (req, res) => {
+    const {nome, preco } = req.body
+
+    if (!nome || preco === undefined) {
+        return res.status(400).json({erro: "Nome e preço são obrigatórios"})
+    }
+    
+    if (typeof preco !== "number") {
+        return res.status(400).json({erro: "Preço deve ser um número"})
+    }
+    
+    if (preco < 0) {
+        return res.status(400).json({erro: "Preço não pode ser negativo"})
+    }
+
+    const novoProduto = {
+        id: produtos.length + 1,
+        nome,
+        preco
+    }
+
+    produtos.push(novoProduto)
+
+    res.status(201).json(novoProduto)
+
+})
+
+app.put("/produtos/:id", (req, res) => {
+    const id = Number(req.params.id)
+    const { nome, preco } = req.body
+
+    const produto = produtos.find(p => p.id === id)
+
+    if (!produto) {
+        return res.status(404).json({ erro: "Produto não encontrado" })
+    }
+
+    if (nome === undefined && preco === undefined) {
+        return res.status(400).json({ erro: "Informe nome ou preço para atualizar" })
+    }
+
+    if (preco !== undefined) {
+        if (typeof preco !== "number") {
+            return res.status(400).json({ erro: "Preço deve ser um número" })
+        }
+
+        if (preco < 0) {
+            return res.status(400).json({ erro: "Preço não pode ser negativo" })
+        }
+
+        produto.preco = preco
+    }
+
+    if (nome !== undefined) {
+        produto.nome = nome
+    }
+
+    res.json({
+        mensagem: "Produto atualizado com sucesso",
+        produto
+    })
+})
+
+app.delete("/produtos/:id", (req, res) => {
+    const id = Number(req.params.id)
+
+    const index = produtos.findIndex(p => p.id === id)
+
+    if (index === -1) {
+        return res.status(404).json({ erro: "Produto não encontrado" })
+    }
+
+    const [produtoRemovido] = produtos.splice(index, 1)
+
+    res.json({
+        mensagem: "Produto removido com sucesso",
+        produtoRemovido
+    })
+
+
+
+})
+
+app.delete("/produtos", (req, res) => {
+    
+    if (produtos.length === 0) {
+        return res.status(404).json({ erro: "Já não possui nenhum produto no array"})
+    }
+    
+    produtos.length = 0
+
+
+    res.json({
+        mensagem: "Todos produtos removidos com sucesso"
+    })
+
+})
+
+
+
 app.get("/buscar", (req, res) => {
     const nome = req.query.nome
 
